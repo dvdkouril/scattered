bb9a7e4cd91d72e0c3468776550c55b893dee614
David Kouril <david.kouril@gmail.com>
2025-02-10 14:00:38 -0500

    comment out unused func for now

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@
 
 import { mat4, vec3 } from "gl-matrix";
 
-async function loadDataFromURL(url: string): Promise<ArrayBuffer | undefined> {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const buffer = await response.arrayBuffer();
-    //return load(buffer, options);
-    return buffer;
-  } catch (err) {
-    let message = "Unknown Error";
-    if (err instanceof Error) message = err.message;
-    console.error(message);
-    return undefined;
-  }
-}
+//async function loadDataFromURL(url: string): Promise<ArrayBuffer | undefined> {
+//  try {
+//    const response = await fetch(url);
+//    if (!response.ok) {
+//      throw new Error(`Response status: ${response.status}`);
+//    }
+//    const buffer = await response.arrayBuffer();
+//    //return load(buffer, options);
+//    return buffer;
+//  } catch (err) {
+//    let message = "Unknown Error";
+//    if (err instanceof Error) message = err.message;
+//    console.error(message);
+//    return undefined;
+//  }
+//}
 
 function prepareCameraMatrix(width: number, height: number): mat4 {
 
