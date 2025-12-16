4950b353213de1783eb0a3fcccb0202b72eb62c1
David Kouril <david.kouril@gmail.com>
2025-12-16 15:42:31 -0500

    remove commented out code

--- lib/main.ts
+++ lib/main.ts
@@ -8,13 +8,9 @@ function processArrow(b: ArrayBuffer, xField?: string, yField?: string, zField?:
   console.log(table.schema);
 
   const columns = table.toColumns();
-  console.log(columns);
-
   const xArr = xField ? columns[xField] as Float32Array : columns.x as Float32Array;
   const yArr = yField ? columns[yField] as Float32Array : columns.y as Float32Array;
   const zArr = zField ? columns[zField] as Float32Array : columns.z as Float32Array;
-
-  //TODO: I guess I don't need to interlace: I can just put these to separate buffers
 
   // TODO: normalize here or in shader? still need to find the bounds
   //const newXArr = normalize(xArr as Float32Array);
@@ -22,33 +18,8 @@ function processArrow(b: ArrayBuffer, xField?: string, yField?: string, zField?:
   //const newZArr = normalize(zArr as Float32Array);
   //console.log(`newXArr: ${newXArr}`);
 
-  //const combinedArr = new Float32Array(3 * newXArr.length);
-  //for (const [i, v] of newXArr.entries()) {
-  //  const x = v;
-  //  const y = newYArr[i];
-  //  const z = newZArr[i];
-  //  combinedArr.set([x, y, z], i * 3);
-  //}
-  //return combinedArr;
   return [xArr, yArr, zArr];
 }
-
-// TODO: I actually can't just normalize the arrays separately, because that'll deform the space
-//function normalize(arr: Float32Array): Float32Array {
-//  let min = Infinity;
-//  let max = -Infinity;
-//  for (const v of arr) {
-//    min = (v < min) ? v : min;
-//    max = (v > max) ? v : max;
-//  }
-//
-//  const newArr = new Float32Array(arr.length);
-//  for (const [i, v] of arr.entries()) {
-//    const newVal = (v - min) / (max - min);
-//    newArr.set([newVal], i);
-//  }
-//  return newArr;
-//}
 
 /**
   * Displays a 3D scatterplot based on .arrow file provided as URL or an array of points.
